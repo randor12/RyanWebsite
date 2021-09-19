4f9d366646e80859d020e12f565253d7d4cbe002
randor12 <rynicholas@vt.edu>
2021-09-18 20:31:09 -0400

    updated requirements, changed the application, and successfully deployed

--- application.py
+++ application.py
@@ -2,34 +2,33 @@ from flask import *
 
 application = Flask(__name__)
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
-app = application
-app.secret_key = 'asdjfksa'
+application.secret_key = 'asdjfksa'
 
-@app.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET', 'POST'])
 def index():
     # GET Request for Index Template 
     print('Home Page')
     return render_template('index.html')
 
-@app.route('/about', methods=['GET', 'POST'])
+@application.route('/about', methods=['GET', 'POST'])
 def about():
     # About page
     print("About Page")
     return render_template('about.html')
 
-@app.route('/experience', methods=['GET', 'POST'])
+@application.route('/experience', methods=['GET', 'POST'])
 def experience():
     # Experience Page
     print('Experience Page')
     return render_template('experience.html')
 
-@app.route('/classes', methods=['GET', 'POST'])
+@application.route('/classes', methods=['GET', 'POST'])
 def classes():
     # Class Page 
     print('Class Info')
     return render_template('classes.html')
 
-@app.route('/contact', methods=['GET', 'POST'])
+@application.route('/contact', methods=['GET', 'POST'])
 def contact():
     # Contact Page 
     print('Contact Info')
